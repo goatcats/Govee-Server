0840e67229bbb855216ae3c9bd925fac2bb5dc67
goatcats <78546026+goatcats@users.noreply.github.com>
2023-07-24 23:13:24 +0200

    Update main.js

--- nodeServer/main.js
+++ nodeServer/main.js
@@ -1,33 +1,29 @@
 const http = require("http");
 const Govee = require("node-govee-led");
 
-const strips = new Govee({
-    apiKey: "614f41d4-204d-404c-980a-9ae395653d1a",
-    mac: "25:D2:A4:C1:38:70:1A:FB",
-    model: "H6144",
-});
-const panels = new Govee({
+const device = new Govee({
     apiKey: "614f41d4-204d-404c-980a-9ae395653d1a",
     mac: "D0:E2:C4:39:32:35:30:49",
     model: "H6061",
 });
 
-let myDevices = [strips, panels];
 let lightsChanged = false;
 
-async function callTheChild(devices) {
-    for (let i = 0; i < devices.length; i++) {
-        const device = devices[i];
-        await device.turnOn();
-        await device.setBrightness(50);
-        await device.setColor("#ff0000");
-    }
+async function callTheChild(device) {
+    const brightness = await device.getBrightness();
+    await device.turnOn();
+    await device.setBrightness(100);
+    await device.setColor("#ff0000");
+    setTimeout(async () => {
+        await device.setColor("0000ff");
+        await device.setBrightness(brightness);
+    }, 10000)
 }
 
 const server = http.createServer((req, res) => {
     if (req.url === "/run" && req.method === "GET") {
         res.setHeader("Access-Control-Allow-Origin", "*");
-        callTheChild(myDevices)
+        callTheChild(device)
             .then(() => {
                 res.statusCode = 200;
                 res.setHeader("Content-Type", "text/plain");
